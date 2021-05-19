119834bc744fbb4a305366a5c21e3dc59c96f163
pedro <pedro.gustavo.lozano.wright@gmail.com>
2021-05-19 16:24:59 -0600

    comit codigo github

--- lib/main.dart
+++ lib/main.dart
@@ -149,7 +149,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     onPressed: () {
                       print("---inicio---");
 
-                      print("hola gustavo");
+                      print("hola GitHub");
 
                       print("---final---");
                     },
